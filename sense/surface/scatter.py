059436b71eefb1d8b141bba3e19f2780f36f2ea2
Thomas Weiß <t.weiss@iggf.geo.uni-muenchen.de>
2017-07-27 13:51:29 +0200

    update

--- sense/surface/scatter.py
+++ sense/surface/scatter.py
@@ -11,4 +11,15 @@ class SurfaceScatter(object):
 
     def _check(self):
         assert isinstance(self.eps, complex)
-    
+
+class SurfaceScatterWaterCloudModel(object):
+    def __init__(self, mv, theta, C=None, C1=None, C2=None, D=None):
+        self.mv = mv
+        self.theta = theta
+        self.C = C
+        self.C1 = C1
+        self.C2 = C2
+        self.D = D
+
+
+
