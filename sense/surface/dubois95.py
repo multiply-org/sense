corrected units (ks) in dubois95

sense/surface/dubois95.py
@@ -23,8 +23,10 @@ class Dubois95(SurfaceScatter):
         self.hv = None
 
     def _calc_sigma(self):
-        lam = self.lam*100. 
-        return self._vv(lam, self.ks), self._hh(lam, self.ks)
+        lam = self.lam*100.  # from m in cm
+        ks = self.ks/100  # This line reproduces ULABY value. s is in cm, here!
+        # (added 10*np.log10() like in SMART_FORWARDMODEL.m)
+        return 10.*np.log10(self._vv(lam, ks)), 10.*np.log10(self._hh(lam, ks))
 
     def _hh(self, lam, ks):
         """
@@ -45,17 +47,17 @@ class Dubois95(SurfaceScatter):
         d = (ks*np.sin(self.theta))**1.1*lam**0.7
 
         return b*c*d
-    
+
     def plot(self):
         f = plt.figure()
         ax = f.add_subplot(111)
         t = np.rad2deg(self.theta)
-        ax.plot(t, 10.*np.log10(self.hh), color='blue', label='hh')
-        ax.plot(t, 10.*np.log10(self.vv), color='red', label='vv')
+        ax.plot(t, self.hh, color='blue', label='hh')
+        ax.plot(t, self.vv, color='red', label='vv')
        # ax.plot(t, 10.*np.log10(self.hv), color='green', label='hv')
         ax.grid()
-        ax.set_ylim(-25.,0.)
-        ax.set_xlim(0.,70.)
+        ax.set_ylim(-35.,-5.)
+        ax.set_xlim(30.,70.)
         ax.legend()
         ax.set_xlabel('incidence angle [deg]')
         ax.set_ylabel('backscatter [dB]')

versuch_oh-dubois.py
@@ -31,7 +31,7 @@ freq = 1.  # GH
 s = 0.05  # m
 ks = (2.*np.pi/f2lam(freq))*s
 
-theta = np.deg2rad(np.arange(0.,70.) )
+theta = np.deg2rad(np.arange(0.,71.) )
 
 O = Oh92(eps, ks, theta)
 O.plot()