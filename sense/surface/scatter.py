381aa5466ad7846cef827b41bc806894e95dae66
McWhity <weiss.thomas@lmu.de>
2018-06-11 17:26:04 +0200

    include class SurfaceScatterWaterCloud in class SurfaceScatter

--- sense/surface/scatter.py
+++ sense/surface/scatter.py
@@ -2,29 +2,21 @@
 Major surface scatter class
 """
 class SurfaceScatter(object):
-    def __init__(self, eps, ks, theta, kl=None, **kwargs):
+    def __init__(self, eps=None, ks=None, theta=None, kl=None, mv=None, C_hh=None, C_vv=None, D_hh=None, D_vv=None, **kwargs):
         self.eps = eps
         self.ks = ks
         self.theta = theta
         self.kl = kl
-        self._check()
 
-    def _check(self):
-        pass
-        # assert isinstance(self.eps, complex)
-
-class SurfaceScatterWaterCloud(object):
-    def __init__(self, mv, theta, C_hh=None, C_vv=None, D_hh=None, D_vv=None):
         self.mv = mv
-        self.theta = theta
         self.C_hh = C_hh
         self.C_vv = C_vv
         self.D_hh = D_hh
         self.D_vv = D_vv
 
+        self._check()
+
     def _check(self):
-        #needs to be implemented
         pass
-
-
+        # assert isinstance(self.eps, complex)
 
